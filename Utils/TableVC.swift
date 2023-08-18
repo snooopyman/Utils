219c87f223f341e7f1ac18155d632a02df782a62
Armando Cáceres <armando.snoop@gmail.com>
2023-08-18 20:06:13 +0200

    first commit: practicing programmatic UI using UIStackView, UIView, UIbutton, UILabel. - need to refact.

--- Utils/TableVC.swift
+++ Utils/TableVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TableVC.swift
 //  Utils
 //
 //  Created by Jesús Armando Cáceres Vilchez on 18/8/23.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TableVC: UIViewController {
+
+    private var tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemPink
     }
-
-
+    
 }
-
